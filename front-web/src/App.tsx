0d1dc2a14c0c5a6d09bacc8dd3ce2dbcfad08d8b
Paulo Roberto <prbasa@gmail.com>
2021-01-10 11:14:49 -0300

    Add navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
 
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <Navbar />
+      </div>
      
      
     </div>
