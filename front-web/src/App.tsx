bf9475a38cfa40bf4e54d3019aac4cf02dd1881b
Paulo Roberto <prbasa@gmail.com>
2021-01-10 11:32:46 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,16 +1,14 @@
 import './App.css';
+import './App.css';
 import Navbar from './Navbar';
-
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <div>
+     <>
         <Navbar />
-      </div>
-     
-     
-    </div>
+        <Home />
+     </>
   );
 }
 
